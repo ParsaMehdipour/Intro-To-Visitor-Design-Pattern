c7e0744263c4c9937e08dbef27df7e8c56f7697e
Parsa <mehdipourparsa@gmail.com>
2024-10-27 17:58:31 -0400

    feat: Add component interfaces

---
+++ DemoLibrary/ComponentInterfaces/IPatient.cs
@@ -0,0 +1,15 @@
+﻿using DemoLibrary.VisitorInterfaces;
+
+namespace DemoLibrary.ComponentInterfaces;
+
+/// <summary>
+/// The Component interface declares an `accept` method that should take the
+/// base visitor interface as an argument.
+/// </summary>
+public interface IPatient
+{
+    string Name { get; set; }
+    int Age { get; set; }
+    string MedicalHistory { get; set; }
+    void Accept(IHealthcareVisitor visitor);
+}
